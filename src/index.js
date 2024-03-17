7b128cbb6a6d88831c05ab73e47df33b0dafd641
satrijo <mail@satrio.dev>
2024-03-17 13:44:53 +0700

    docker always restart and add cors policy

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import fetchData from "./lib/scrape.js";
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 3113;
 
+app.use(cors());
 app.get("/", (req, res) => {
     res.json({
         status: "success",
